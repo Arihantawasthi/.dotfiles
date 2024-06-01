c3be1ff2406efd3a941177ce2b00cc548e1718c2
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-06-02 02:47:52 +0530

    added LSP Attach

--- nvim/lua/daemon/init.lua
+++ nvim/lua/daemon/init.lua
@@ -1,2 +1,60 @@
 require("daemon.set")
 require("daemon.lazy_init")
+
+local augroup = vim.api.nvim_create_augroup
+local DaemonGroup = augroup('Daemon', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+
+
+function M(name)
+    require("plenary.reload").reload_module(name)
+end
+
+vim.filetype.add({
+    extension = {
+        templ = "templ",
+    }
+})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = "*",
+    callback = function ()
+        vim.highlight.on_yank({
+            higroup = "IncSearch",
+            timeout = 40,
+        })
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = DaemonGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+autocmd('LspAttach', {
+    group = DaemonGroup,
+    callback = function (e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.impementation() end, opts)
+        vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+        vim.keymap.set("n", "<leader>D", function() vim.lsp.buf.type_definition() end, opts)
+        vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.diagnostic.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    end
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25

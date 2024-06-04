dc053a47b7085239aed1a367054c2c97b024c4f7
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-06-04 22:53:02 +0530

    Added installation script

---
+++ install.sh
@@ -0,0 +1,5 @@
+ln -s ~/.dotfiles/tmux ~/.config/tmux
+ln -s ~/.dotfiles/startship.toml ~/.config/startship.toml
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.zprofile ~/.zprofile
+ln -s ~/.dotfiles/zsh ~/.config/zsh

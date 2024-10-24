1b99fffbb0f39d2d8e0819ed2c9cd80813525b00
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-10-24 20:27:11 +0530

    feat: adjusted window height, width and position; fix: relative line numbers; fix: made the buffer unmodifiable

--- nvim/lua/seefunc/lua/seefunc.lua
+++ nvim/lua/seefunc/lua/seefunc.lua
@@ -14,12 +14,15 @@ local function inspect(o)
 end
 
 local function create_floating_win(filename)
-    local width = vim.api.nvim_get_option("columns")
+    -- local width = vim.api.nvim_get_option("columns")
     local height = vim.api.nvim_get_option("lines")
-    local min_width = math.ceil(width * 0.8)
+    -- local min_width = math.ceil(width * 0.8)
     local min_height = math.ceil(height * 0.8)
-    local row = math.ceil((height - min_height) / 2)
-    local col = math.ceil((width - min_width) / 2)
+    -- local row = math.ceil((height - min_height) / 2)
+    -- local col = math.ceil((width - min_width) / 2)
+    local row = 1
+    local col = 2
+    local min_width = 70
 
     local opts = {
         relative = 'editor',
@@ -35,10 +38,9 @@ local function create_floating_win(filename)
     local buf = vim.api.nvim_create_buf(false, true)
     local win = vim.api.nvim_open_win(buf, true, opts)
 
-    -- vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
-    -- vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-    vim.api.nvim_win_set_option(win, 'number', true)
-    vim.api.nvim_win_set_option(win, 'relativenumber', true)
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe') -- delete from memory when buf is hidden
+    vim.api.nvim_buf_set_option(buf, 'relativenumber', true)
+    vim.api.nvim_buf_set_option(buf, 'number', true)
 
     return buf, win
 end
@@ -63,7 +65,11 @@ M.see_implementation = function()
         local buf, win = create_floating_win(filename)
         vim.api.nvim_buf_set_lines(buf, 0, -1, false, file_content)
 
-        vim.api.nvim_command("edit " .. filename)
+        local filetype = vim.fn.fnamemodify(filename, ":e")
+        vim.api.nvim_buf_set_option(buf, "filetype", filetype)
+
+        vim.api.nvim_buf_set_option(buf, "modifiable", false)
+        vim.api.nvim_buf_set_option(buf, "readonly", true)
 
         vim.api.nvim_win_set_cursor(win, { def.targetSelectionRange.start.line + 1, def.targetSelectionRange.start.character })
         vim.api.nvim_set_keymap('n', '<leader>gD', ':lua require"seefunc".see_implementation()<CR>',

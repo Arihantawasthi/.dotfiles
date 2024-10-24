de8c139c7440a59341def3a94cb6f04b0ee15563
Arihant <34059418+Arihantawasthi@users.noreply.github.com>
2024-10-24 17:05:32 +0530

    feat: rendered the file contents into the buffer window

--- nvim/lua/daemon/lazy/seefunc.lua
+++ nvim/lua/daemon/lazy/seefunc.lua
@@ -3,7 +3,7 @@ return {
         dir = "~/.config/nvim/lua/seefunc",
         name = "seefunc",
         config = function ()
-            vim.api.nvim_set_keymap("n", "<leader>gD", ':lua require"seefunc".see_implementation()<CR>', { noremap = true, silent = true })
+            vim.api.nvim_set_keymap('n', '<leader>gD', ':lua require"seefunc".see_implementation()<CR>', { noremap = true, silent = true })
         end
     },
 }
---
+++ nvim/lua/seefunc/lua/seefunc.lua
@@ -0,0 +1,74 @@
+local M = {}
+
+local function inspect(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            s = s .. '[' .. k .. '] = ' .. inspect(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
+local function create_floating_win(filename)
+    local width = vim.api.nvim_get_option("columns")
+    local height = vim.api.nvim_get_option("lines")
+    local min_width = math.ceil(width * 0.8)
+    local min_height = math.ceil(height * 0.8)
+    local row = math.ceil((height - min_height) / 2)
+    local col = math.ceil((width - min_width) / 2)
+
+    local opts = {
+        relative = 'editor',
+        title = filename,
+        width = min_width,
+        height = min_height,
+        row = row,
+        col = col,
+        style = 'minimal',
+        border = 'rounded'
+    }
+
+    local buf = vim.api.nvim_create_buf(false, true)
+    local win = vim.api.nvim_open_win(buf, true, opts)
+
+    -- vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+    -- vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    vim.api.nvim_win_set_option(win, 'number', true)
+    vim.api.nvim_win_set_option(win, 'relativenumber', true)
+
+    return buf, win
+end
+
+M.see_implementation = function()
+    local params = vim.lsp.util.make_position_params()
+    vim.lsp.buf_request(0, 'textDocument/definition', params, function(_, result)
+        if not result or vim.tbl_isempty(result) then
+            print("No definition found!")
+            return
+        end
+
+        local def = result[1]
+        if not def or not def.targetSelectionRange or not def.targetSelectionRange.start then
+            print("Invalid definition structure!")
+            return
+        end
+
+        local filename = vim.uri_to_fname(def.targetUri)
+        local file_content = vim.fn.readfile(filename)
+
+        local buf, win = create_floating_win(filename)
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, file_content)
+
+        vim.api.nvim_command("edit " .. filename)
+
+        vim.api.nvim_win_set_cursor(win, { def.targetSelectionRange.start.line + 1, def.targetSelectionRange.start.character })
+        vim.api.nvim_set_keymap('n', '<leader>gD', ':lua require"seefunc".see_implementation()<CR>',
+            { noremap = true, silent = true })
+    end)
+end
+
+return M
